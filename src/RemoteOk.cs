5baa2ef10b51e90d5705f1c99e98718e6ce34c4f
huok <77536370+dee-shar@users.noreply.github.com>
2025-11-03 21:14:22 +0300

    Create RemoteOk.cs

---
+++ src/RemoteOk.cs
@@ -0,0 +1,22 @@
+using System.Net.Http;
+
+namespace RemoteOkApi
+{
+    public class RemoteOk
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://remoteok.com/api";
+        public RemoteOk()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36");
+        }
+
+        public async Task<string> GetJobs()
+        {
+            var response = await httpClient.GetAsync(apiUrl);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
